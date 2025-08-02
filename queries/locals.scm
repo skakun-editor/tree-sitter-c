ea0ce1045eccfce51909f1bd9f05ebc911475a77
Karol Łacina <karol@lacina.io>
2025-08-02 21:37:05 +0200

    fix: limit locals queries to function parameters only

--- queries/locals.scm
+++ queries/locals.scm
@@ -1,34 +1,8 @@
 ;; Scopes
 
-; TODO: Modify the grammar to add function_definition_scope
-
-[(translation_unit)
- (compound_statement)] @local.scope
+(function_definition) @local.scope
 
 ;; Definitions
-
-; HACK: Matches only up to T var[…][…][…][…][…][…][…] = {…} but higher levels
-;       of nesting are extremely rare (i.e. not found in Linux's source code),
-;       so it's okay.
-(declaration declarator:
- [(identifier) @local.definition
-  (_ declarator:
-   [(identifier) @local.definition
-    (_ declarator:
-     [(identifier) @local.definition
-      (_ declarator:
-       [(identifier) @local.definition
-        (_ declarator:
-         [(identifier) @local.definition
-          (_ declarator:
-           [(identifier) @local.definition
-            (_ declarator:
-             [(identifier) @local.definition
-              (_ declarator:
-               [(identifier) @local.definition
-                (_ declarator: (identifier) @local.definition)])])])])])])])])
-
-(preproc_def name: _ @local.definition)
 
 ; HACK: Matches only up to T(*param)[…][…][…], T**(*param)(…) and T*****param
 ;       but higher levels of nesting are extremely rare (i.e. not found in
@@ -47,10 +21,6 @@
            (_ declarator:
             [(identifier) @local.definition
              (_ declarator: (identifier) @local.definition)])])])])]))))
-
-(preproc_function_def name: _ @local.definition)
-
-(labeled_statement label: _ @local.definition)
 
 ;; References
 
